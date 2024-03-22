aeb5d45a5e53925df8c6466e5e040a90117500fe
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-22 14:35:58 +0000

    add component which houses fullscreen map

---
+++ components/FullscreenMap.tsx
@@ -0,0 +1,34 @@
+import ShopMap from "./ShopMap";
+
+export default function FullscreenMap() {
+    const initialRegion = {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.08,
+        longitudeDelta: 0.08,
+    };
+
+    const coffeeShops = [
+        {
+            latitude: 37.779,
+            longitude: -122.43,
+        },
+        {
+            latitude: 37.789,
+            longitude: -122.435,
+        },
+        {
+            latitude: 37.7904,
+            longitude: -122.432,
+        },
+    ];
+
+    return (
+        <ShopMap
+            initialRegion={initialRegion}
+            coffeeShops={coffeeShops}
+            width="full"
+            height="full"
+        ></ShopMap>
+    );
+}
