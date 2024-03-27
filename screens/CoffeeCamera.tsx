5fe628ff0e3768a80c0b7f85aa0c3fd24769dfe9
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-27 15:43:55 +0000

    remove unused import

--- screens/CoffeeCamera.tsx
+++ screens/CoffeeCamera.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { Text, View, Image, Pressable } from "react-native";
 import { Camera, CameraType } from "expo-camera";
