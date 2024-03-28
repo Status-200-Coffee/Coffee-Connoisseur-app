f13ec1e90b5ead08ce30a9cc274199097b9238b0
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-28 10:09:31 +0000

    format all files

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -11,7 +11,6 @@ export default function LoadingScreen({ navigation }: Props<"LoadingScreen">) {
     const { cache, setCache } = useCache();
     const [retry, setRetry] = useState(0);
 
-
     useEffect(() => {
         getUserLocation()
             .then((result) => {
@@ -26,102 +25,99 @@ export default function LoadingScreen({ navigation }: Props<"LoadingScreen">) {
                     return "Carlisle";
                 }
 
+                const { latitude, longitude } = result;
 
-        const { latitude, longitude } = result;
+                setCache((cache) => {
+                    return { ...cache, userLocation: { latitude, longitude } };
+                });
 
-        setCache((cache) => {
-          return { ...cache, userLocation: { latitude, longitude } };
-        });
+                return getClosestCity({ latitude, longitude });
+            })
+            .then((city) => {
+                setCache((cache) => {
+                    return { ...cache, currentCity: city };
+                });
 
-        return getClosestCity({ latitude, longitude });
-      })
-      .then((city) => {
-        setCache((cache) => {
-          return { ...cache, currentCity: city };
-        });
+                return getCities();
+            })
+            .then((cities) => {
+                const newObj: CityRegions = {};
 
-        return getCities();
-      })
-      .then((cities) => {
-        const newObj: CityRegions = {};
+                cities.map((city) => {
+                    newObj[city.city] = {
+                        latitude: city.latitude,
+                        longitude: city.longitude,
+                        latitudeDelta: 0.04,
+                        longitudeDelta: 0.04,
+                    };
+                });
 
-        cities.map((city) => {
-          newObj[city.city] = {
-            latitude: city.latitude,
-            longitude: city.longitude,
-            latitudeDelta: 0.04,
-            longitudeDelta: 0.04,
-          };
-        });
+                setCache((cache) => {
+                    return { ...cache, cities: newObj };
+                });
+            })
+            .then(() => {
+                setCache((cache) => {
+                    return { ...cache, cityShops: {} };
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                if (retry > 5) {
+                    throw new Error("Too many retries");
+                }
 
-        setCache((cache) => {
-          return { ...cache, cities: newObj };
-        });
-      })
-      .then(() => {
-        setCache((cache) => {
-          return { ...cache, cityShops: {} };
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        if (retry > 5) {
-          throw new Error("Too many retries");
-        }
+                console.log(retry);
 
-        console.log(retry);
+                gotEverything();
+            });
+    }, [retry]);
 
-        gotEverything();
-      });
-  }, [retry]);
-
-
-  function gotEverything() {
-    if (
-      cache.userLocation &&
-      cache.cities &&
-      cache.currentCity &&
-      cache.cityShops
-    ) {
-      navigation.navigate("ShopSearch");
-    } else {
-      setRetry((retry) => retry + 1);
-
-    }
-  }
-
-  return (
-    <View className="flex-1 flex justify-end">
-      <Image
-        className="w-full h-full absolute"
-        source={require("../assets/WelcomePageImage3.jpg")}
-      />
-      <LinearGradient
-        colors={["transparent", "rgba(21, 21, 21, 0.95)"]}
-        style={{ width: 700, height: 700 }}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-        className="absolute bottom-0"
-      />
-      <View className="p-10 pb inset-x-0 bottom-20">
-        <Text className="text-center text-4xl font-bold text-white space-y-8">
-          Welcome to Coffee Connoisseur
-        </Text>
-        <View className="space-y-3 p-2"></View>
-        <TouchableOpacity
-          onPress={() => {
+    function gotEverything() {
+        if (
+            cache.userLocation &&
+            cache.cities &&
+            cache.currentCity &&
+            cache.cityShops
+        ) {
             navigation.navigate("ShopSearch");
-          }}
-          className="bg-white opacity-90 mx-auto p-4 px-10 rounded-full"
-        >
-          <Text className="font-bold text-xl text-slate-900">
-            Discover Coffee
-          </Text>
-        </TouchableOpacity>
-        <StatusBar backgroundColor={"rgba(21, 21, 21, 0.95)"} />
-      </View>
-    </View>
-  );
+        } else {
+            setRetry((retry) => retry + 1);
+        }
+    }
+
+    return (
+        <View className="flex-1 flex justify-end">
+            <Image
+                className="w-full h-full absolute"
+                source={require("../assets/WelcomePageImage3.jpg")}
+            />
+            <LinearGradient
+                colors={["transparent", "rgba(21, 21, 21, 0.95)"]}
+                style={{ width: 700, height: 700 }}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+                className="absolute bottom-0"
+            />
+            <View className="p-10 pb inset-x-0 bottom-20">
+                <Text className="text-center text-4xl font-bold text-white space-y-8">
+                    Welcome to Coffee Connoisseur
+                </Text>
+                <View className="space-y-3 p-2"></View>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("ShopSearch");
+                    }}
+                    className="bg-white opacity-90 mx-auto p-4 px-10 rounded-full"
+                >
+                    <Text className="font-bold text-xl text-slate-900">
+                        Discover Coffee
+                    </Text>
+                </TouchableOpacity>
+                <StatusBar backgroundColor={"rgba(21, 21, 21, 0.95)"} />
+            </View>
+        </View>
+    );
 }
