519869585a9cf0bfb27f32914c5b30e23e4e1891
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-04-03 09:51:01 +0100

    allow user to vote only once

--- components/ShopRating.tsx
+++ components/ShopRating.tsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
 import StarRating from "react-native-star-rating";
+
 import { useCache } from "../contexts/Cache";
 import { updateShopRating } from "../utils/api";
+
 import { ShopRatingProps } from "./types";
 
-
-export default function ShopRating({ shop_id, setRating, setVotes, shopPage }:ShopRatingProps) {
+export default function ShopRating({
+    shop_id,
+    setRating,
+    setVotes,
+    shopPage,
+}: ShopRatingProps) {
     const { cache } = useCache();
     const [userRating, setUserRating] = useState(shopPage.userVote);
     const [userHasVoted, setUserHasVoted] = useState(false);
 
     const handleRating = async (stars: number) => {
+        setUserHasVoted(true);
         setUserRating(stars);
         if (!userHasVoted && !userRating) {
             const update = await updateShopRating(
-                cache.currentCity,
+                cache.currentCity!,
                 shop_id,
                 stars
             );
             setRating(update.rating);
             setVotes(update.totalRatings);
-            setUserHasVoted(true);
             shopPage.rating = update.rating;
             shopPage.totalRatings = update.totalRatings;
         }
@@ -31,7 +37,7 @@ export default function ShopRating({ shop_id, setRating, setVotes, shopPage }:Sh
 
     return cache.user ? (
         <StarRating
-            disabled={false}
+            disabled={userHasVoted}
             maxStars={5}
             rating={userRating}
             selectedStar={(stars: number) => handleRating(stars)}
